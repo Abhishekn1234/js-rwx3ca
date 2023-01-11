284b37c5d3becd88afe1e29d78508616fbc90bb8
Abhishekn1234 <111786351+Abhishekn1234@users.noreply.github.com>
2023-01-11 14:24:00 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,104 @@
-// Import stylesheets
-import './style.css';
+let testCase1 = [
+  {
+    name:"leo messi",
+    chemistry:91,
+    biology:91,
+    dob: "31-12-1995",
+  },
+  {
+    name: "Cristiano Ronaldo",
+    biology: 90,
+    chemistry: 90,
+    dob: "31-05-1992",
+  },
+  {
+    name: "Virat Kohli",
+    biology: 96,
+    chemistry: 96,
+    dob: "31-12-1995",
+  },
+  {
+    name: "Rohit Sharma",
+    biology: 86,
+    chemistry: 86,
+    dob: "31-12-1995",
+  },
+  {
+    name: "Viswanathan Anand",
+    biology: 99,
+    chemistry: 99,
+    dob: "12-12-1994",
+  },
+];
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+// ans = [virat, leo, cristiano, rohit, viswanath anand]
+
+let testCase2 = [
+  {
+    name: "Leo Messi",
+    dob: "31-12-1995",
+    biology: 100,
+    chemistry: 80,
+  },
+  {
+    name: "Cristiano Ronaldo",
+    dob: "31-05-1992",
+    biology: 80,
+    chemistry: 100,
+  },
+  {
+    name: "Virat Kohli",
+    dob: "31-12-1995",
+    biology: 30,
+    chemistry: 40,
+  },
+  {
+    name: "Rohit Sharma",
+    dob: "31-12-1995",
+    biology: 40,
+    chemistry: 30,
+  },
+  {
+    name: "Viswanathan Anand",
+    dob: "12-12-1994",
+    biology: 99,
+    chemistry: 10,
+  },
+];
+
+// ans = [leo, cristiano, viswanath, rohit, virat]
+
+let testcase3 =[
+  {
+    name: "lion",
+    dob: "31-12-1995",
+    biology: 100,
+    chemistry: 80,
+  },
+  {
+    name: "Crypto",
+    dob: "31-05-1992",
+    biology: 80,
+    chemistry: 100,
+  },
+  {
+    name: "Veena",
+    dob: "31-12-1995",
+    biology: 30,
+    chemistry: 40,
+  },
+  {
+    name:"Rope",
+    dob: "31-12-1995",
+    biology: 40,
+    chemistry: 30,
+  },
+  {
+    name: "Anand",
+    dob: "12-12-1994",
+    biology: 99,
+    chemistry: 10,
+  },
+];
+
+// ans = [lion, crypto, veena, rope, anand]
